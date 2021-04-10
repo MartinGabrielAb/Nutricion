d2343a376c263defbd68eb62cb66a5cd7faa032d
Paninowsky <pablo2443@gmail.com>
2021-04-09 22:27:27 -0300

    Commit prueba

--- app/Alimento.php
+++ app/Alimento.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alimento extends Model
 {
-	/*Changesss*/
+	/*Cambio asd*/
     protected $table = 'alimento';
 	protected $primaryKey = 'AlimentoId';
 	public $timestamps = false;
