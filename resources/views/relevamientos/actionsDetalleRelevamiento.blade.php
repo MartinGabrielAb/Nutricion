638c29c593019c9158280dfbd4f9cfee7686219e
MartinGabrielAb <67345132+MartinGabrielAb@users.noreply.github.com>
2021-05-04 15:44:14 -0300

    Limpieza de código 2

--- resources/views/relevamientos/actionsDetalleRelevamiento.blade.php
+++
@@ -1,8 +0,0 @@
-<button type="button" class="btn btn-sm btn-default " data-toggle="modal" onclick="cargarSelect2({{$DetalleRelevamientoId}})" data-target="#modalEditarDetalleRelevamiento{{$DetalleRelevamientoId}}">
-  Editar
-</button>
-@include('relevamientos.modal.editarDetalleRelevamiento')
-<button type="button" class="btn btn-sm btn-default " data-toggle="modal" data-target="#modalEliminarDetalleRelevamiento{{$DetalleRelevamientoId}}">
-  Eliminar
-</button>
-@include('relevamientos.modal.eliminarDetalleRelevamiento')
